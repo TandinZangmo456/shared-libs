343f4d82eb0567c4fd496e1f5b379e8615224cbf
TandinZangmo <02230305.cst@rub.edu.bt>
2025-06-03 01:09:37 +0600

    draft1

--- vars/nodePipeline.groovy
+++ vars/nodePipeline.groovy
@@ -30,7 +30,7 @@ def call(Map config = [:]) {
 
             stage('Push Docker Image') {
                 steps {
-                    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+                    withCredentials([usernamePassword(credentialsId: 'penguintandinzangmo', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
                         sh """
                             echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
                             docker push ${DOCKER_IMAGE}
